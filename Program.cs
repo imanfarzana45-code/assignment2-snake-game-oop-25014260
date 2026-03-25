28e28088ae65b73c66ee7aecef24dc2d787d277c
imanfarzana45-code <imanfarzana45@gmail.com>
2026-03-25 18:37:51 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -8,12 +8,10 @@ namespace snakegame
         {
             Console.WriteLine("Snake Game Demo");
 
-            // Create objects
             Position start = new Position(5, 5);
             Snake snake = new Snake(start);
             Food food = new Food(10, 10);
 
-            // Show initial state
             Console.WriteLine("Initial Snake Position:");
             foreach (var p in snake.Body)
             {
@@ -22,7 +20,6 @@ namespace snakegame
 
             Console.WriteLine($"Food at: ({food.Pos.X}, {food.Pos.Y})");
 
-            // Move snake
             snake.Move(1, 0);
 
             Console.WriteLine("After moving:");
@@ -31,7 +28,6 @@ namespace snakegame
                 Console.WriteLine($"({p.X}, {p.Y})");
             }
 
-            // Interaction: eat food
             if (snake.Body[0].X == food.Pos.X && snake.Body[0].Y == food.Pos.Y)
             {
                 snake.Grow();
